8f917e40bab05901ef57e22c10942950ec07665f
Cheryl L <cheryllewman@gmail.com>
2019-07-26 07:03:46 -0700

    updated detail view

---
+++ Bike Gallery/BikeGallery/Controllers/BikeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BikeGallery.Controllers
+{
+    public class BikeController : Controller
+    {
+        public ActionResult Detail()
+        {
+            ViewBag.SeriesTitle = "Fort Stevens";
+            ViewBag.IssueNumber = "1";
+            ViewBag.Description = "<p> a description of the ride at Fort Stevens.</p>";
+            ViewBag.Riders = new string[]
+                {
+                "Mom: Cheryl",
+                "Dad:  Jason",
+                "Child 1:  Ash",
+                "Child 2: Sage"
+                };
+
+            return View();
+        }
+    }  
+
+}
