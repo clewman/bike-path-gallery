a20a6c8c8006d2cf640d52072fb9f27ee4c020e5
Cheryl L <cheryllewman@gmail.com>
2019-07-26 08:06:28 -0700

    added bike path and rider models

---
+++ Models/Rider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BikeGallery.Models
+{
+    public class Rider
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
