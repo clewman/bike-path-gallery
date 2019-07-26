17fae62b3c77499400bb2f894a58d75c9c1fc2d4
Cheryl L <cheryllewman@gmail.com>
2019-07-26 08:12:14 -0700

    updated model

--- Models/BikePath.cs
+++ Models/BikePath.cs
@@ -13,5 +13,23 @@ namespace BikeGallery.Models
         public string DescriptionHtml { get; set; }
         public Rider[] Riders { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        //series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-")
+                    .ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
